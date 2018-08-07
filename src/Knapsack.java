cae1e1097b416be92a1a77ddaddf76eba80c7d3f
Bayron Cardenas <ingenialex96@gmail.com>
2018-08-07 14:49:00 -0500

    Refactor was completed

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,26 +1,41 @@
-public class Knapsack {
+class Knapsack {
 
     private int capacity;
     private int[] weight;
     private int[] value;
 
-    public Knapsack(int capacity, int[] weight, int[] value) {
+
+    Knapsack(int capacity, int[] weight, int[] value) {
         this.capacity = capacity;
         this.weight = weight;
         this.value = value;
     }
 
-    private int max(int a, int b) {
-        return a > b ? a : b;
+    int calculateMaxProfit()
+    {
+        int[][] K= calculateProfitMatrix();
+        return K[value.length][capacity];
     }
 
-    public int[] maxProfit() {
+    int[] obtainListItems(int profit)
+    {
+        int[] items = new int[value.length];
+        for (int i = value.length - 1; i >= 0; i--) {
+            if (value[i] <= profit) {
+                items[i] = 1;
+                profit -= value[i];
+            } else {
+                items[i] = 0;
+            }
+        }
+        return items;
+    }
+
+    private int[][] calculateProfitMatrix() {
         int n = value.length + 1;
-        int i;
-        int w = 0;
         int[][] K = new int[n][capacity + 1];
-        for (i = 0; i < n; i++) {
-            for (w = 0; w <= capacity; w++) {
+        for (int i = 0; i < n; i++) {
+            for (int w = 0; w <= capacity; w++) {
                 if (i == 0 || w == 0) {
                     K[i][w] = 0;
                 } else {
@@ -32,16 +47,10 @@ public class Knapsack {
                 }
             }
         }
-        int profit = K[i - 1][w - 1];
-        int[] items = new int[value.length];
-        for (i = value.length - 1; i >= 0; i--) {
-            if (value[i] <= profit) {
-                items[i] = 1;
-                profit -= value[i];
-            } else {
-                items[i] = 0;
-            }
-        }
-        return items;
+        return K;
+    }
+
+    private int max(int a, int b) {
+        return a > b ? a : b;
     }
 }
--- src/Maze.java
+++ src/Maze.java
@@ -1,47 +1,60 @@
-public class Maze {
+class Maze {
 
     private int[][] maze;
     private int[] posInitial;
     private int[] posFinal;
+    private int[][] solve;
 
-    public Maze(int[][] maze, int[] posInitial, int[] posFinal) {
+    Maze(int[][] maze, int[] posInitial, int[] posFinal) {
         this.maze = maze;
         this.posInitial = posInitial;
         this.posFinal = posFinal;
+        this.solve = new int[maze.length][maze.length];
+    }
+
+    int[][] SolveMaze() {
+        move(posInitial[0], posInitial[1]);
+        return solve;
+    }
+
+    private void move(int x, int y) {
+
+        if (isValidPath(x, y)) { solve[x][y] = 1; }
+        moveRight(x, y);
+        moveLeft(x, y);
+        moveUp(x, y);
+        moveDown(x, y);
     }
 
     private boolean isValidPath(int x, int y) {
-        if (x >= 0 && x < maze.length && y >= 0 && y < maze.length) {
-            if (maze[x][y] != 1) {
-                return true;
-            }
-        }
-        return false;
+        return (x >= 0 && x < maze.length && y >= 0 && y < maze.length && maze[x][y]!=1);
     }
 
-    public int[][] SolveMaze() {
-        int[][] solve = new int[maze.length][maze.length];
-        return move(posInitial[0], posInitial[1], solve);
+    private void moveRight(int x, int y) {
+        if (isValidPath(x, y + 1) && solve[x][y + 1] != 1 && notIsExit(x, y)) {
+            move(x, y + 1);
+        }
     }
 
-    private int[][] move(int x, int y, int[][] solve) {
-        int escape=0;
-        if (isValidPath(x, y)) {
-            if(x==posFinal[0] && y==posFinal[1]){escape=1;}
-            solve[x][y] = 1;
+    private void moveLeft(int x, int y) {
+        if (isValidPath(x, y - 1) && solve[x][y - 1] != 1 && notIsExit(x, y)) {
+            move(x, y - 1);
         }
-        if (isValidPath(x, y + 1) && solve[x][y + 1] != 1 && escape==0) {
-            move(x, y + 1, solve);
+    }
+
+    private void moveUp(int x, int y) {
+        if (isValidPath(x + 1, y) && solve[x + 1][y] != 1 && notIsExit(x, y)) {
+            move(x + 1, y);
         }
-        if (isValidPath(x, y - 1) && solve[x][y - 1] != 1 && escape==0) {
-            move(x, y - 1, solve);
+    }
+
+    private void moveDown(int x, int y) {
+        if (isValidPath(x - 1, y + 1) && solve[x - 1][y] != 1 && notIsExit(x, y)) {
+            move(x - 1, y);
         }
-        if (isValidPath(x + 1, y) && solve[x + 1][y] != 1 && escape==0) {
-            move(x + 1, y, solve);
-        }
-        if (isValidPath(x - 1, y + 1) && solve[x - 1][y] != 1 && escape==0) {
-            move(x - 1, y, solve);
-        }
-        return solve;
+    }
+
+    private boolean notIsExit(int x, int y) {
+        return !(x == posFinal[0] && y == posFinal[1]);
     }
 }
